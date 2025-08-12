dd20bd43cdc185d9ca541d87b898e61433b35c96
WangXL1024 <wangxlisonglm@163.com>
2025-08-12 13:49:13 +0800

    delete test java

--- backend/target/generated-sources/annotations/com/postion/airlineorderbackend/mapper/OrderMapperImpl.java
+++ backend/target/generated-sources/annotations/com/postion/airlineorderbackend/mapper/OrderMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-12T13:27:48+0800",
+    date = "2025-08-12T13:48:56+0800",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.50.v20250729-0351, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
